579cd18eb2a7f6c71e066889e6e6b83c7b79cac3
Jery Javier Sirei Mena <jery_sirei@ucne.edu.do>
2021-01-14 09:57:21 -0400

    Add project files.

---
+++ Ejercicio1Cap1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio1Cap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hola Mi nombre es Jery Javier");
+        }
+    }
+}
