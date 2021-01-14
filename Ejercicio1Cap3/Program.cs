8d83e38226feee982f622a8ad29b66acffc4d666
JerySiri <77344266+JerySiri@users.noreply.github.com>
2021-01-14 15:10:09 -0400

    Add files via upload

---
+++ Ejercicio1Cap3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ejercicio1Cap3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            Console.WriteLine("Introdusca un numero: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if(num % 2 == 0)
+            {
+                Console.WriteLine("{0} es un numero par.", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} es un numero impar.", num);
+            }
+        }
+    }
+}
---
+++ Ejercicio4Cap3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Ejercicio4Cap3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            Console.WriteLine("Introduca un numero del 1 al 7: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("--Domingo--");
+                break;
+
+                case 2:
+                    Console.WriteLine("--Lunes--");
+                break;
+
+                case 3:
+                    Console.WriteLine("--Martes--");
+                break;
+
+                case 4:
+                    Console.WriteLine("--Miercoles--");
+                break;
+
+                case 5:
+                    Console.WriteLine("--Jueves--");
+                break;
+
+                case 6:
+                    Console.WriteLine("--Viernes--");
+                break;
+
+                case 7:
+                    Console.WriteLine("--Sabado--");
+                break;
+
+                default:
+                    Console.WriteLine("Error, Laopcion que tomo no es correcta.");
+                break;
+            }
+
+        }
+    }
+}
