47383a595e42e128f8b2b1b0ec0176aa0eb6b802
JerySiri <77344266+JerySiri@users.noreply.github.com>
2021-01-14 10:04:44 -0400

    Add files via upload

---
+++ Ejercicio5Cap1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ejercicio5Cap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mi nombre es Jery Javier.");
+            Console.Write("Que al todo?");
+        }
+    }
+}
